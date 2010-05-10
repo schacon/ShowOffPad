533842e378e074b55d69ced0a4c9a54dbff559bb
Scott Chacon <schacon@gmail.com>
2010-05-10 11:53:12 -0700

    almost

--- Classes/ShowOffPadAppDelegate.m
+++ Classes/ShowOffPadAppDelegate.m
@@ -20,41 +20,36 @@
 	//Code to detect if an external display is connected to the iPad.
 	NSLog(@"Number of screens: %d", [[UIScreen screens]count]);
 	
-	//Now, if there's an external screen, we need to find its modes, itereate through them and find the highest one. Once we have that mode, break out, and set the UIWindow.
+	presentController = [[ShowOffPadPresentController alloc] 
+						 initWithNibName:@"ShowOffPadPresentController" 
+						 bundle:nil];
+	viewController.extDisplay = presentController.mainView;
 	
 	if([[UIScreen screens]count] > 1) //if there are more than 1 screens connected to the device
 	{
-		CGSize max;
-		UIScreenMode *maxScreenMode;
+		UIScreenMode *bestScreenMode;
 		for(int i = 0; i < [[[[UIScreen screens] objectAtIndex:1] availableModes]count]; i++)
 		{
 			UIScreenMode *current = [[[[UIScreen screens]objectAtIndex:1]availableModes]objectAtIndex:i];
-			if(current.size.width > max.width);
-			{
-				max = current.size;
-				maxScreenMode = current;
+			if((current.size.width == 1024) && (current.size.height == 768)) {
+				bestScreenMode = current;
 			}
 		}
 		
 		//Now we have the highest mode. Turn the external display to use that mode.
 		UIScreen *external = [[UIScreen screens] objectAtIndex:1];
-		external.currentMode = maxScreenMode;
+		external.currentMode = bestScreenMode;
 		
 		//Boom! Now the external display is set to the proper mode. We need to now set the screen of a new UIWindow to the external screen
 		//extWindow = [[UIWindow alloc] init];
 		extWindow = [[UIWindow alloc] initWithFrame:[external bounds]];
 
-		presentController = [[ShowOffPadPresentController alloc] 
-							 initWithNibName:@"ShowOffPadPresentController" 
-							 bundle:nil];
-		
 		[extWindow addSubview:presentController.view];
 		[extWindow makeKeyAndVisible];
 		
-		
 		extWindow.screen = external;
 	}	
-
+		
     [window addSubview:viewController.view];
     [window makeKeyAndVisible];
 	
--- Classes/ShowOffPadPresentController.m
+++ Classes/ShowOffPadPresentController.m
@@ -13,36 +13,21 @@
 
 @synthesize mainView;
 
-/*
- // The designated initializer.  Override if you create the controller programmatically and want to perform customization that is not appropriate for viewDidLoad.
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
-    if ((self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil])) {
-        // Custom initialization
-    }
-    return self;
-}
-*/
-
-/*
-// Implement loadView to create a view hierarchy programmatically, without using a nib.
-- (void)loadView {
-}
-*/
-
 // Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
 - (void)viewDidLoad {
 	NSLog(@"Mofo Loaded");
+	
     [super viewDidLoad];
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
-    if (interfaceOrientation == UIInterfaceOrientationLandscapeLeft) {
+	if (interfaceOrientation == UIInterfaceOrientationLandscapeLeft) {
 		return YES;
 	}
     if (interfaceOrientation == UIInterfaceOrientationLandscapeRight) {
 		return YES;
 	}
-	return NO;
+	return YES;
 }
 
 - (void)didReceiveMemoryWarning {
